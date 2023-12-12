efb0d7b4586320faf5cfdd63e4942870d44ce278
Alessandro-Brag <152172391+Alessandro-Brag@users.noreply.github.com>
2023-12-12 10:21:30 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 class Main {
+
   public static void main(String[] args) {
+    String parolachiave;
+    System.out.println("inserire parola chiave");
     System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
     Matrice m = new Matrice("TPSIT");
     Vigenere v = new Vigenere (0, 12, 0, 12, m); 
     Thread t = new Thread(v);
      new Thread(v).start();
+    //new commento
   }
 }
